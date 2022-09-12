7cd570c8bcac8cda7366e8c9046dade140310aaf
Jaime Moura <jaimemoura>
2022-09-12 13:03:02 -0400

    atualizações tela de cadastro e animações

---
+++ src/app/acesso/acesso.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
+
+@Component({
+  selector: 'app-acesso',
+  templateUrl: './acesso.component.html',
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style ({
+        opacity: 1
+      })),
+      transition('void => criado',[
+        style({opacity: 0, transform: 'translate(-50px, 0)' }),
+        animate('500ms 1s ease-in-out') //duração, delay e aceleração
+      ])
+      ]),
+      trigger('animacao-painel', [
+        state('criado', style ({
+          opacity: 1
+        })),
+        transition('void => criado',[
+          style({opacity: 0, transform: 'translate(50px, 0)' }),
+          animate('500ms 1s ease-in-out') //duração, delay e aceleração
+          ])
+      ])
+  ]
+})
+export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'
+  public estadoPainel: string = 'criado'
+
+  public cadastro: boolean = true
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
